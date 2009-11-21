279121bd1210aab9ee31a7835d1390e62f1709c4
James Liu <james.ch.liu@gmail.com>
2009-11-20 17:24:10 -0800

    initial file

---
+++ src/futils/futils.clj
@@ -0,0 +1,8 @@
+(ns utils.files
+  #^{:author "James Liu",
+     :doc "File utilities"}
+  (:use [ clojure.contrib.duck-streams ]))
+(defn find 
+  "Return all files"
+  [file]
+  (file-seq (file-str file))
