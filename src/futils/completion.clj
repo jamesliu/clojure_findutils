29a7e7b7cce7e2ee09e55c8d5f9585b2b8370f98
James Liu <james.ch.liu@gmail.com>
2009-11-21 22:42:44 -0800

    completion script

---
+++ src/futils/completion.clj
@@ -0,0 +1,8 @@
+(def completions
+  (reduce concat (map (fn [p] (keys (ns-publics (find-ns p))))
+    '(clojure.core clojure.set clojure.xml clojure.zip clojure.contrib.duck-streams))))
+
+(with-open [f (java.io.BufferedWriter. (java.io.FileWriter. (str (or (System/getenv "HOME") (System/getenv "HOMEPATH")) "/.clj_completions")))]
+    (.write f (apply str (interleave completions (repeat "\n")))))
+
+
