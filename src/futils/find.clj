59e8eebe551ba92ec7523261a99e0d7a84866f16
James Liu <james.ch.liu@gmail.com>
2009-11-22 23:27:41 -0800

    refactor regex

--- src/futils/find.clj
+++ src/futils/find.clj
@@ -1,7 +1,10 @@
-(ns futils.find
-  #^{:author "James Liu",
+(ns 
+  #^{:author "James Liu"
      :doc "File utilities"}
-  (:use (clojure.contrib.duck-streams )))
+  futils.find 
+  (:use clojure.contrib.duck-streams clojure.contrib.str-utils clojure.contrib.java-utils)
+  (:import (java.io File)))
+
 (defn find-files
   "Return all directories and files"
   [file]
@@ -20,43 +23,4 @@
           :when (re-find regex name)]
       name)))
 
-(defn first-match [m]
-  (if (coll? m) (first m) m))
-
-(defn match [regex text]
-  (let [m (first-match (re-find (re-pattern regex) text))]
-    (if (nil? m)
-      [0 0]
-      (let [ind (.indexOf text m) len (.length m)]
- [ind (+ ind len)]))))
-
-(defn regexcoach []
-  (let [frame (JFrame. "Regular Expression Coach") pane (JPanel.) regexText (JTextField.) 
- targetText (JTextField. "")
- statusBar (JLabel. "Match from 0 to 0")
-        keyHandler (proxy [KeyAdapter] [] 
-       (keyTyped [keyEvent] 
-         (try       
-   (let [m (match (.getText regexText) (.getText targetText)) 
-         hl (.getHighlighter targetText)
-         pen (DefaultHighlighter$DefaultHighlightPainter. Color/RED)]
-     (.removeAllHighlights hl)
-     (.addHighlight hl (first m) (second m) pen)
-     (.setText statusBar (format "Match from %s to %s" (first m) (second m))))
-   (catch PatternSyntaxException e (.setText statusBar (.getMessage e))))))]
-    (doto regexText
-      (.addKeyListener keyHandler))
-    (doto targetText
-      (.addKeyListener keyHandler))
-    (doto pane
-      (.setLayout (BoxLayout. pane BoxLayout/Y_AXIS))
-      (.add (JLabel. "Regular Expression"))
-      (.add regexText)
-      (.add (JLabel. "Target String"))
-      (.add targetText)
-      (.add statusBar))
-    (doto frame
-      (.add pane)
-      (.setSize 300 300)
-      (.setVisible true))))
 
