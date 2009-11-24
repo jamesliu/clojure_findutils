df90473ddadb34e8728f6925b5a917a1adf5e0a1
James Liu <james.ch.liu@gmail.com>
2009-11-24 14:58:53 -0800

    remove find-classes

--- src/futils/find.clj
+++ src/futils/find.clj
@@ -10,17 +10,3 @@
   [file]
   (file-seq (file-str file)))
 
-(defn find-classes [regex]
-  (let [search-path-properities ["java.class.path" "sun.boot.class.path"]]
-    (for [search-path search-path-properities
-          jar (filter #(.endsWith % ".jar")
-                      (.split (System/getProperty search-path)
-                              (System/getProperty "path.separator")))
-          entry (try (filter #(.endsWith (.getName %) ".class")
-                             (enumeration-seq (.entries (new java.util.jar.JarFile jar))))
-                     (catch Exception _))
-          name [(.. entry getName (replaceAll "\\.class$" ""))]
-          :when (re-find regex name)]
-      name)))
-
-
