566339791176dc29a441c1819c2cc0d2bc317f2b
James Liu <james.ch.liu@gmail.com>
2009-11-24 14:59:44 -0800

    remove futils.clj

--- src/futils/futils.clj
+++
@@ -1,8 +0,0 @@
-(ns utils.files
-  #^{:author "James Liu",
-     :doc "File utilities"}
-  (:use [ clojure.contrib.duck-streams ]))
-(defn find 
-  "Return all files"
-  [file]
-  (file-seq (file-str file))
