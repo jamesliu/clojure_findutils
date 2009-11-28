19ba8a3d99b82f9e402ee325e3f7fdef03219a92
James Liu <james.ch.liu@gmail.com>
2009-11-27 22:50:43 -0800

    add gen-class

--- src/futils/find.clj
+++ src/futils/find.clj
@@ -1,12 +1,29 @@
 (ns 
   #^{:author "James Liu"
      :doc "File utilities"}
-  futils.find 
-  (:use clojure.contrib.duck-streams clojure.contrib.str-utils clojure.contrib.java-utils)
+  futils.find
+  (:gen-class)
+  (:use clojure.contrib.duck-streams 
+        clojure.contrib.str-utils    
+        clojure.contrib.java-utils 
+        clojure.contrib.command-line)
   (:import (java.io File)))
 
 (defn find-files
   "Return all directories and files"
   [file]
   (file-seq (file-str file)))
+
+(defn -main [& args]
+  (with-command-line args
+    "Unix find command"
+    [[foo "This is the description for foo" 1]
+     [bar "This is the description for bar" 2]
+     [boolean? b? "This is a boolean flag."]
+     remaining]
+    (println "foo: " foo)
+    (println "bar: " bar)
+    (println "boolean?: " boolean?)
+    (println "remaining: " remaining)))
+
 
