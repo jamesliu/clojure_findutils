e6cad8cce22dd8119db99060348f612d7512279b
James Liu <james.ch.liu@gmail.com>
2009-11-28 15:55:48 -0800

    add filter

--- src/futils/find.clj
+++ src/futils/find.clj
@@ -14,16 +14,36 @@
   [file]
   (file-seq (file-str file)))
 
+(defn filter-filename
+  "Return the filtered filename"
+  [re files]
+  (filter #(re-find re (.getName %)) files))
+
+(defn filter-filetype
+  [ftype files]
+  (cond 
+    (= ftype nil) files
+    (= ftype "d") (filter #(.isDirectory %) files)
+    (= ftype "f") (filter #(.isFile %) files)
+    :else files))
+
+(defn addfilter
+  [fn & args]
+  (comp (apply partial fn args) ufind))
+
+(def ufind find-files)
+
 (defn -main [& args]
   (with-command-line args
     "Unix find command"
-    [[foo "This is the description for foo" 1]
-     [bar "This is the description for bar" 2]
-     [boolean? b? "This is a boolean flag."]
+    [[name "file name" ".*"]
+     [type "file type"]
+     [help? h? "help"]
      remaining]
-    (println "foo: " foo)
-    (println "bar: " bar)
-    (println "boolean?: " boolean?)
-    (println "remaining: " remaining)))
+    ;(println "dir: " (first remaining))
+    ;(println "name: " name)
+    (def ufind (addfilter filter-filename (re-pattern name)))
+    (def ufind (addfilter filter-filetype type))
+    (println (ufind (first remaining)))))
 
 
